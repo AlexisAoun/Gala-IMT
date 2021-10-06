68a26cb8c84f0f4595e1ff54b507eec376a5eab9
Alexis Aoun <alexis.aoun@hotmail.com>
2021-10-06 20:31:03 +0200

    Add: navbar for home page

--- js/navbar.js
+++ js/navbar.js
@@ -41,4 +41,47 @@ class Navbar extends HTMLElement {
   }
 }
 
+class NavbarHome extends HTMLElement {
+  constructor() {
+    super();
+    this.innerHTML = `
+    <div class="navbar">
+      <li>
+        <ul>
+          <a href="#">
+            Accueil
+          </a>
+        </ul>
+        <ul>
+          <a href="html/programme.html">
+            Programme
+          </a>
+        </ul>
+        <ul>
+          <a href="html/billeterie.html">
+            Billeterie
+          </a>
+        </ul>
+        <ul>
+          <a href="html/logistique.html">
+            Logistique
+          </a>
+        </ul>
+        <ul>
+          <a href="html/sponsors.html">
+            Sponsors
+          </a>
+        </ul>
+        <ul>
+          <a href="html/don.html">
+            Dons
+          </a>
+        </ul>
+      </li>
+    </div>                                     
+
+      `;
+  }
+}
 customElements.define("navbar-menu", Navbar);
+customElements.define("navbar-home", NavbarHome);
