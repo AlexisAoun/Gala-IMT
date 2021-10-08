b2a92922a5394475345d812c394a7094b088554b
Alexis Aoun <alexis.aoun@hotmail.com>
2021-10-08 02:42:30 +0200

    Add: completed home page with fixes to footer

---
+++ js/common.js
@@ -0,0 +1,30 @@
+// code for the home page
+
+//change the carte image on mouse hover
+function changeImage(element) {
+  if(element.id == "programme") {
+     element.src = "assets/cartes_accueil/programme_carte_bleue.png";
+  } else if (element.id == "billeterie") {
+     element.src = "assets/cartes_accueil/billeterie_carte_bleue.png";
+  } else if (element.id == "logistique") {
+     element.src = "assets/cartes_accueil/logistique_carte_bleue.png";
+  } else if (element.id == "sponsors") {
+     element.src = "assets/cartes_accueil/sponsors_carte_bleue.png";
+  } else if (element.id == "don") {
+     element.src = "assets/cartes_accueil/faites_un_don_carte_bleue.png";
+  }
+}
+
+function resetImage(element) {
+  if(element.id == "programme") {
+     element.src = "assets/cartes_accueil/programme_carte.png";
+  } else if (element.id == "billeterie") {
+     element.src = "assets/cartes_accueil/billeterie_carte.png";
+  } else if (element.id == "logistique") {
+     element.src = "assets/cartes_accueil/logistique_carte.png";
+  } else if (element.id == "sponsors") {
+     element.src = "assets/cartes_accueil/sponsors_carte.png";
+  } else if (element.id == "don") {
+     element.src = "assets/cartes_accueil/faites_un_don_carte.png";
+  }
+}
