e8cf73552a7d45fcb73b143cc6d4e302496fd6d4
Alexis Aoun <alexis.aoun@hotmail.com>
2021-12-05 10:59:44 +0100

    feat: programme page and flavivon

--- js/footer.js
+++ js/footer.js
@@ -4,6 +4,43 @@ class PageFooter extends HTMLElement {
     this.innerHTML = `
     <div class="footer-container">
       <div class="footer-logo">
+        <img src="../assets/logos/simple_logo.png">
+      </div>
+      <div class="blocLeft"></div>
+      <div class="footer-social-media">
+        <p>Suivez nous sur les réseaux sociaux!</p> 
+        <div class="logoReseaux">
+          <a href="#"><i class="fa fa-facebook-square"></i></a>
+          <a href="#"><i class="fa fa-instagram"></i></a>
+          <a href="#"><i class="fa fa-linkedin"></i></a>
+          <a href="#"><i class="fa fa-youtube"></i></a>
+        </div>
+      </div>
+      <div class="footer-info">
+        <div class="text1">
+          <p>Le Comité du Gala 2022</p>
+          <p>IMT Nord Europe</p>
+        </div>
+        <div class="text2">
+          <p>Rendez-vous le 05 Février 2022</p>
+          <p>Au Casinode Saint-Amand-Les-Eaux</p>
+        </div>
+      </div>
+      <div class="blocRight"></div>
+      <div class="footer-sponsors">
+        <img src="../assets/logos/edf_white.webp">
+      </div>
+    </div>
+      `;
+  }
+}
+
+class PageFooterHome extends HTMLElement {
+  constructor() {
+    super();
+    this.innerHTML = `
+    <div class="footer-container">
+      <div class="footer-logo">
         <img src="assets/logos/simple_logo.png">
       </div>
       <div class="blocLeft"></div>
@@ -37,3 +74,4 @@ class PageFooter extends HTMLElement {
 
 // Define the new element
 customElements.define("page-footer", PageFooter);
+customElements.define("page-footer-home", PageFooterHome);
