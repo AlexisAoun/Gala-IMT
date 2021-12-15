e926fadf6226d04c79aaf5f04cd9dfb22c23e246
Alexis Aoun <alexis.aoun@hotmail.com>
2021-12-15 10:45:05 +0100

    feat(sponsor): logo tweaks

--- js/footer.js
+++ js/footer.js
@@ -30,7 +30,7 @@ class PageFooter extends HTMLElement {
       <div class="footer-sponsors">
         <a href="sponsors.html" target="_blank">
           <img class="s1" src="../assets/logos/logo_alumni.png">
-          <img class="s2" src="../assets/logos/edf.jpg">
+          <img class="s2" src="../assets/logos/edf.png">
           <img class="s3" src="../assets/logos/mrod.png">
           <img class="s4" src="../assets/logos/ergo.png">
           <img class="s5" src="../assets/logos/urbania.jpg">
